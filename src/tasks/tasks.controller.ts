f68cf72b7fab55471b2ccb99c8aae80ab531bfad
Truong Duc Khai <duckhai80@gmail.com>
2025-10-30 19:53:47 +0700

    Implementing PATCH requests

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,12 +4,14 @@ import {
   Get,
   NotFoundException,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { CreateTaskDto } from './create-task.dto';
 import { FindOneParams } from './find-one.params';
-import { Task } from './tasks.model';
+import type { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
+import { UpdateTaskStatusDto } from './update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -21,18 +23,36 @@ export class TasksController {
   }
 
   @Get(':id')
-  public findOne(@Param() params: FindOneParams) {
-    const task = this.tasksService.findOne(params.id);
+  public findOne(@Param() params: FindOneParams): Task {
+    const task = this.findOneOrFail(params.id);
+
+    return task;
+  }
+
+  @Post()
+  public create(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.create(createTaskDto);
+  }
+
+  @Patch(':id/status')
+  public updateStatus(
+    @Param() params: FindOneParams,
+    @Body() body: UpdateTaskStatusDto,
+  ): Task {
+    const task = this.findOneOrFail(params.id);
+
+    task.status = body.status;
+
+    return task;
+  }
+
+  private findOneOrFail(id: string): Task {
+    const task = this.tasksService.findOne(id);
 
     if (!task) {
       throw new NotFoundException();
     }
 
     return task;
-  }
-
-  @Post()
-  public create(@Body() createTaskDto: CreateTaskDto) {
-    return this.tasksService.create(createTaskDto);
   }
 }
