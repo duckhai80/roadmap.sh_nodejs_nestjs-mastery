234da7021d831a73876d6591167f744b729f5608
Truong Duc Khai <duckhai80@gmail.com>
2025-10-31 19:58:05 +0700

    Using ConfigService in our services

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { DummyService } from './dummy/dummy.service';
 import { LoggerService } from './logger/logger.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly DummyService: DummyService,
     private readonly loggerService: LoggerService,
+    private readonly configService: ConfigService,
   ) {}
 
   getHello(): string {
+    const messagePrefix = this.configService.get<string>('app.messagePrefix');
+
     return this.loggerService.log(
-      `Hello World! ${this.DummyService.getData()}`,
+      `${messagePrefix} Hello World! ${this.DummyService.getData()}`,
     );
   }
 }
