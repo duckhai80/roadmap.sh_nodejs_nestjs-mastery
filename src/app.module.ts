8bc13f41bf1dda6311dc8001088d307f770b9d6a
Truong Duc Khai <duckhai80@gmail.com>
2025-10-31 19:41:12 +0700

    Install and setting up NestJS config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { appConfig } from './config/app.config';
 import { DummyService } from './dummy/dummy.service';
-import { MessageFormatterService } from './message-formatter/message-formatter.service';
 import { LoggerService } from './logger/logger.service';
+import { MessageFormatterService } from './message-formatter/message-formatter.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService, DummyService, MessageFormatterService, LoggerService],
 })
